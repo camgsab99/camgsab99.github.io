86f900602b8ca6cd064aaf3232c49302900d16eb
Cameron Sabiston <camgsab99@live.com>
2022-09-30 02:32:42 -0400

    First Iteration

---
+++ commands.js
@@ -0,0 +1,81 @@
+var twitter = "https://www.twitter.com/camgsab99/";
+var password = "camsab";
+var linkedin = "https://www.linkedin.com/in/cameron-sabiston-11753999/";
+var instagram = "https://www.instagram.com/og.cammysabby/";
+var github = "https://github.com/camgsab99/";
+var email = "mailto:camgsab99@live.com";
+
+whois = [
+  "<br>",
+  "Hey, I'm Cam!👋",
+  "I'm a student at the East Carolina University and I'm currently majoring in Computer Science.",
+  "<br>",
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>",
+];
+
+social = [
+  "<br>",
+    "You can find me on the following social media platforms:",
+  'twitter        <a href="' +
+    twitter +
+    '" target="_blank">twitter/camgsab99' +
+    "</a>",
+  'linkedin       <a href="' +
+    linkedin +
+    '" target="_blank">linkedin/cameronsabiston/' +
+    "</a>",
+  'instagram      <a href="' +
+    instagram +
+    '" target="_blank">instagram/og.cammysabby' +
+    "</a>",
+  'github         <a href="' +
+    github +
+    '" target="_blank">github/camgsab99' +
+    "</a>",
+  "<br>",
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Cameron?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">CammySabby</span>',
+  "  _____                                        _____       _     _     _                ",  
+  " / ____|                                      / ____|     | |   (_)   | |               ",
+  " | |     __ _ _ __ ___   ___ _ __ ___  _ __   | (___   __ _| |__  _ ___| |_ ___  _ __   ",
+  " | |    / _` | '_ ` _ \ / _ \ '__/ _ \| '_ \   \___ \ / _` | '_ \| / __| __/ _ \| '_ \  ",
+  " | |___| (_| | | | | | |  __/ | | (_) | | | |  ____) | (_| | |_) | \__ \ || (_) | | | | ",
+  " \_____\__,_|_| |_| |_|\___|_|  \___/|_| |_| |_____/ \__,_|_.__/|_|___/\__\___/|_| |_|  ",
+  "                                                                                        ",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+];
