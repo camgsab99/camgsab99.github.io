751af4a2c260f456aa4d02751173870e68d75798
camgsab99 <camgsab99@live.com>
2022-12-14 18:07:54 -0500

    Update

---
+++ JavaScript/commands.js
@@ -0,0 +1,70 @@
+var youtube = "https://www.youtube.com/@TheElectriqulGaming/";
+var twitter = "https://www.twitter.com/camgsab99/";
+var password = "WeezyFBaby";
+var linkedin = "https://www.linkedin.com/in/cameronsabiston//";
+var instagram = "https://www.instagram.com/og.cammysabby/";
+var github = "https://github.com/camgsab99/";
+var email = 'cameronsabiston@gmail.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Cameron!👋",
+  "I'm a software developer from North Carolina",
+  "I'm currently working on a few projects, including a new website.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/electriqul' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/cameronsabiston' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/cameronsabiston' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/cameronsabiston' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/cameronsabiston' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Cameron?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">CameronSabiston (CS) Not A Corporation. All rights reserved.</span>',
+  "   ___                                       __       _     _     _               ", 
+  "  / __\\__ _ _ __ ___   ___ _ __ ___  _ __   / _\\ __ _| |__ (_)___| |_ ___  _ __   ",
+  " / /  / _` | '_ ` _ \\ / _ \\ '__/ _ \\| '_ \\  \\ \\ / _` | '_ \\| / __| __/ _ \\| '_ \\  ",
+  "/ /__| (_| | | | | | |  __/ | | (_) | | | | _\\ \\ (_| | |_) | \\__ \\ || (_) | | | | ",
+  "\\____/\\__,_|_| |_| |_|\\___|_|  \\___/|_| |_| \\__/\\__,_|_.__/|_|___/\\__\\___/|_| |_| ",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
